e597c27e35ede1acd3e55a8bc3306c24d353fd9e
Samrudhi Ambekar <samrudhiambekar4@gmail.com>
2026-02-12 15:02:03 +0530

    fix: configure eslint for TypeScript support

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -2,11 +2,17 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
-    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
 };
