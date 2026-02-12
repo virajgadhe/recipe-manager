3cbdcd22b0da3164904a06a1dec7145bc5b3e3a0
Samrudhi Ambekar <samrudhiambekar4@gmail.com>
2026-02-12 12:09:04 +0530

    fix: configure ESLint to support TypeScript parsing

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -4,9 +4,15 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
 };
