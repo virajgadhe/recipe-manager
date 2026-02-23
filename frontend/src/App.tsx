18e4ee506baadbe5db7578954ba72824640c0dd2
Samrudhi Ambekar <samrudhiambekar4@gmail.com>
2026-02-23 12:24:50 +0530

    setup public pages folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <div>Frontend setup complete</div>;
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import RecipeDetail from './pages/RecipeDetail';
+import CategoryPage from './pages/CategoryPage';
+import SearchPage from './pages/SearchPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes/:id" element={<RecipeDetail />} />
+      <Route path="/categories/:id" element={<CategoryPage />} />
+      <Route path="/search" element={<SearchPage />} />
+    </Routes>
+  );
 }
+
+export default App;
