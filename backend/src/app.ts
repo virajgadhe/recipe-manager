13c0e8f241f321b332b879dcef463a60b423979d
Samrudhi Ambekar <samrudhiambekar4@gmail.com>
2026-03-04 12:17:48 +0530

    fix express v5 wildcard route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -46,7 +46,7 @@ const frontendPath = path.join(__dirname, '../../frontend/dist');
 
 app.use(express.static(frontendPath));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
